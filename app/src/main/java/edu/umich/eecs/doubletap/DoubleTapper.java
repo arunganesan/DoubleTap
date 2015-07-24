27bb3912d8369e1906b6aa2d4dd0817ac0f12dbb
Arun Ganesan <arunganesan123@gmail.com>
2015-07-24 17:45:22 -0400

    Initial commit

---
+++ app/src/main/java/edu/umich/eecs/doubletap/DoubleTapper.java
@@ -0,0 +1,147 @@
+package edu.umich.eecs.doubletap;
+
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.os.Environment;
+import android.util.Log;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.ArrayList;
+
+/**
+ * Created by Arun on 2/11/2015.
+ */
+public class DoubleTapper {
+    private MyActivity activity;
+    private SensorManager sm;
+    private Sensor accelerometer;
+    public boolean saved = false;
+    public boolean debugging = false;
+    private static String FOLDER = "soundfield";
+    private long last_triggered = 0;
+    private int trigger_delay = 5000;
+    private float THRESH = 2;
+
+    private int min_peak_delay = 150;
+    private int max_peak_delay = 600;
+    private long last_peak = 0;
+
+    private String TAG = "DTapper";
+    int bufferIndex = 0;
+    private float [] cb = new float [100];
+    private float [] smoothed = new float [100];
+    public ArrayList<ArrayList<Double>> debugData = new ArrayList<ArrayList<Double>>();
+
+    public DoubleTapper (MyActivity activity) {
+        this.activity = activity;
+        sm = (SensorManager) activity.getSystemService(Context.SENSOR_SERVICE);
+        accelerometer = sm.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        sm.registerListener(accelListener, accelerometer, SensorManager.SENSOR_DELAY_FASTEST);
+    }
+
+    public void triggerEvent () {
+        if (System.currentTimeMillis() > last_triggered + trigger_delay) {
+            last_triggered = System.currentTimeMillis();
+            (new Thread(eventTrigger)).start();
+        }
+    }
+
+    Runnable eventTrigger = new Runnable() {
+        @Override
+        public void run() {
+            for (int i = 0; i < 4; i++) {
+                try { Thread.currentThread().sleep(250); } catch (Exception e) { }
+                activity.flagStart();
+                try { Thread.currentThread().sleep(250); } catch (Exception e) { }
+                activity.flagEnd();
+            }
+        }
+    };
+
+    /**
+     * Spawn a new thread for accelerometer processing.
+     */
+
+    private String getFilename() {
+        String filepath = Environment.getExternalStorageDirectory().getPath();
+        File file = new File(filepath, FOLDER);
+        if(!file.exists()) file.mkdirs();
+        return file.getAbsolutePath() + "/recording." + System.currentTimeMillis() + ".txt";
+    }
+
+    public void saveDebug () {
+        if (saved) return; saved = true;
+        FileOutputStream out;
+        String outFilename = getFilename();
+        try {
+            out = new FileOutputStream(outFilename);
+            out.write("t\tx\ty\tz\n".getBytes());
+            for (ArrayList<Double> row : debugData) {
+                String line = "" + row.get(0) + "\t" + row.get(1) + "\t" + row.get(2) + "\t" + row.get(3) + "\n";
+                out.write(line.getBytes());
+            }
+            out.close();
+            activity.updateText("Done saving to " + outFilename);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    private SensorEventListener accelListener = new SensorEventListener() {
+        public void onAccuracyChanged (Sensor sensor, int accuracy) {}
+        public void onSensorChanged (SensorEvent event) {
+            float x = event.values[0], y = event.values[1], z = Math.abs(event.values[2]);
+            cb[++bufferIndex % cb.length] = z;
+            if (Math.abs(z - cb[(bufferIndex-1) % cb.length]) > THRESH) {
+                long curr_time = System.currentTimeMillis();
+                Log.v(TAG, "Thresh exceeded at " + curr_time);
+                activity.flagStart();
+                //if (curr_time > last_peak + min_peak_delay && curr_time < last_peak + max_peak_delay) {
+                    //triggerEvent();
+                //}
+                last_peak = curr_time;
+            } else {
+                activity.flagEnd();
+                //Log.v(TAG, "Buffer index is " + bufferIndex + " Diff is z is " + z + " and prev is " + (cb[(bufferIndex-1) % cb.length]) + " and diff is "  + Math.abs(z - cb[(bufferIndex-1) % cb.length]));
+            }
+
+            //THRESH
+
+            //for (int i = 0; i < cb.length; i++) sum += cb[i]; average = sum / cb.length;
+            //for (int i = 0; i < cb.length; i++) v += Math.pow(cb[i] - average, 2); v /= cb.length;
+
+            //activity.sayVar(v);
+            //if (v > 100) triggerEvent();
+
+            if (debugging) {
+                if (debugData.size() > 1000) {
+                    //shutdown();
+                    //activity.updateText("Done experiment");
+                    saveDebug();
+                    triggerEvent();
+                } else {
+                    ArrayList<Double> row = new ArrayList<Double>();
+                    row.add((double) System.currentTimeMillis());
+                    row.add((double) x);
+                    row.add((double) y);
+                    row.add((double) z);
+                    debugData.add(row);
+                }
+            }
+        }
+    };
+
+    public void shutdown () {
+        sm.unregisterListener(accelListener);
+    }
+}
---
+++ app/src/main/java/edu/umich/eecs/doubletap/MyActivity.java
@@ -0,0 +1,106 @@
+package edu.umich.eecs.doubletap;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.FrameLayout;
+import android.widget.TextView;
+
+
+public class MyActivity extends Activity {
+    private DoubleTapper doubleTapper;
+    private TextView textView;
+    private FrameLayout frame;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my);
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+
+        wireUI();
+
+        doubleTapper = new DoubleTapper(this);
+        //doubleTapper.startMonitor();
+    }
+
+    private void wireUI () {
+        textView = (TextView) findViewById(R.id.textView);
+        frame = (FrameLayout) findViewById(R.id.parentView);
+        ((Button) findViewById(R.id.recordButton)).setOnClickListener(recordListener);
+    }
+
+    public int f (float i) { return (int)i*100; }
+    public void sayAccel (final float x, final float y, final float z) {
+        updateText("(" + f(x) + ", " + f(y) + ", " + f(z) + ")");
+    }
+
+    public void sayVar (final float variance) {
+        updateText("Variance: " + variance);
+    }
+
+
+    public void updateText (final String msg) {
+        runOnUiThread(new Runnable() {
+            public void run () {
+                textView.setText(msg);
+            }
+        });
+    }
+
+    public void flagStart () {
+        runOnUiThread(new Runnable() {
+            public void run () {
+                frame.setBackgroundColor(0xFFFF0000);
+            }
+        });
+    }
+
+    public void flagEnd () {
+        runOnUiThread(new Runnable() {
+            public void run () {
+                frame.setBackgroundColor(0xFFFFFFFF);
+            }
+        });
+    }
+
+    @Override
+    public void onDestroy () {
+        super.onDestroy();
+        doubleTapper.shutdown();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.my, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+
+    View.OnClickListener recordListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            doubleTapper.debugging = true;
+            doubleTapper.saved = false;
+            doubleTapper.debugData.clear();
+            updateText("Running experiment");
+        }
+    };
+}
